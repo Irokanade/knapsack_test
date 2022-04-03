18b0bf7e6caaba49eed99dade7770386a2c5c923
irokanade <michaeljs.leong@gmail.com>
2022-04-03 23:07:06 +0800

    Initial Commit

---
+++ knapsack_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  knapsack_test
+//
+//  Created by Michael Leong on 03/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
